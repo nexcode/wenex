2fa9a074df7ac446cbd290e2543b77489197700f
nexcode <nexcode@ya.ru>
2016-09-13 09:28:58 +0300

    Work in progress

--- error.go
+++ error.go
@@ -3,9 +3,9 @@ package wenex
 import "errors"
 
 var (
-	ErrHandlerType      = errors.New("Handler must be http.Handler or func(http.ResponseWriter, *http.Request)")
+	ErrHandlerType      = errors.New("Handler must be «http.Handler» or «func(http.ResponseWriter, *http.Request)»")
 	ErrParsePattern     = errors.New("Pattern is not valid")
 	ErrGetFromConfig    = errors.New("ErrGetFromConfig")
-	ErrConfigListenType = errors.New("\"server.http(s).listen\" must be a string type")
-	ErrNoServers        = errors.New("No servers to run. Set \"server.http(s).listen\" in configuration file")
+	ErrConfigListenType = errors.New("Configuration value «server.http(s).listen» must be a string type")
+	ErrNoServers        = errors.New("No servers to run. Set «server.http(s).listen» in configuration file")
 )
