8adefa3969d528befcf5b22e3167756ce206cf7e
Oleg Trifonov <nexcode@ya.ru>
2017-12-26 14:42:20 +0300

    Work in progress

--- config.go
+++ config.go
@@ -113,6 +113,31 @@ func (c *Config) Set(key string, value interface{}) {
 		i++
 	}
 
+	switch value.(type) {
+	case int:
+		value = float64(value.(int))
+	case int8:
+		value = float64(value.(int8))
+	case int16:
+		value = float64(value.(int16))
+	case int32:
+		value = float64(value.(int32))
+	case int64:
+		value = float64(value.(int64))
+	case uint:
+		value = float64(value.(uint))
+	case uint8:
+		value = float64(value.(uint8))
+	case uint16:
+		value = float64(value.(uint16))
+	case uint32:
+		value = float64(value.(uint32))
+	case uint64:
+		value = float64(value.(uint64))
+	case float32:
+		value = float64(value.(float32))
+	}
+
 	data[path[i]] = value
 }
 
