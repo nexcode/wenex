dded3c2f19fe86021179d0d300b88b4618f4102e
Oleg Trifonov <nexcode@ya.ru>
2018-06-01 11:27:37 +0300

    Work in progress

--- run.go
+++ run.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
+func GetRun(ctx context.Context) *Run {
+	runInterface := ctx.Value(ctxRun)
+
+	if runInterface == nil {
+		return nil
+	}
+
+	if run, ok := runInterface.(*Run); ok {
+		return run
+	}
+
+	return nil
+}
+
 func newRun(w http.ResponseWriter, r *http.Request, handler []http.Handler) *Run {
 	run := &Run{
 		rWriter: w,
