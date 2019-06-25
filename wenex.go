f465ef0ccbc077bda84bb42ac97ab03a943004f2
Oleg Trifonov <nexcode@ya.ru>
2019-06-25 12:55:21 +0300

    server close and shutdown

--- wenex.go
+++ wenex.go
@@ -1,10 +1,13 @@
 package wenex
 
 import (
+	"context"
 	"log"
 	"net"
 	"net/http"
 	"path"
+	"sync"
+	"time"
 )
 
 // Wenex struct
@@ -84,31 +87,144 @@ func (wnx *Wenex) Run() error {
 	}
 
 	wnx.fixEmptyChain()
-	stop := make(chan error)
+
+	var (
+		err [2]error
+		wg  sync.WaitGroup
+	)
 
 	if wnx.servers[0] != nil {
+		wg.Add(1)
+
 		go func() {
-			stop <- wnx.servers[0].ListenAndServe()
+			defer wg.Done()
+			err[0] = wnx.servers[0].ListenAndServe()
 		}()
 	}
 
 	if wnx.servers[1] != nil {
+		wg.Add(1)
+
 		go func() {
-			crt, err := wnx.Config.String("server.https.crt")
-			if err != nil {
-				stop <- err
+			defer wg.Done()
+
+			var (
+				crt string
+				key string
+			)
+
+			crt, err[1] = wnx.Config.String("server.https.crt")
+			if err[1] != nil {
+				return
 			}
 
-			key, err := wnx.Config.String("server.https.key")
-			if err != nil {
-				stop <- err
+			key, err[1] = wnx.Config.String("server.https.key")
+			if err[1] != nil {
+				return
 			}
 
-			stop <- wnx.servers[1].ListenAndServeTLS(crt, key)
+			err[1] = wnx.servers[1].ListenAndServeTLS(crt, key)
 		}()
 	}
 
-	return <-stop
+	wg.Wait()
+
+	for _, err := range err {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Close immediately closes all active net.Listeners and any
+// connections in state StateNew, StateActive, or StateIdle. For a
+// graceful shutdown, use Shutdown.
+//
+// Close does not attempt to close (and does not even know about)
+// any hijacked connections, such as WebSockets.
+//
+// Close returns any error returned from closing the Server's
+// underlying Listener(s).
+func (wnx *Wenex) Close() error {
+	var (
+		err [2]error
+		wg  sync.WaitGroup
+	)
+
+	for i := 0; i <= 1; i++ {
+		if wnx.servers[i] != nil {
+			wg.Add(1)
+
+			go func(i int) {
+				defer wg.Done()
+				err[i] = wnx.servers[i].Close()
+			}(i)
+		}
+	}
+
+	wg.Wait()
+
+	for _, err := range err {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Shutdown gracefully shuts down the server without interrupting any
+// active connections. Shutdown works by first closing all open
+// listeners, then closing all idle connections, and then waiting
+// indefinitely for connections to return to idle and then shut down.
+// If the provided context expires before the shutdown is complete,
+// Shutdown returns the context's error, otherwise it returns any
+// error returned from closing the Server's underlying Listener(s).
+//
+// When Shutdown is called, Serve, ListenAndServe, and
+// ListenAndServeTLS immediately return ErrServerClosed. Make sure the
+// program doesn't exit and waits instead for Shutdown to return.
+//
+// Shutdown does not attempt to close nor wait for hijacked
+// connections such as WebSockets. The caller of Shutdown should
+// separately notify such long-lived connections of shutdown and wait
+// for them to close, if desired. See RegisterOnShutdown for a way to
+// register shutdown notification functions.
+//
+// Once Shutdown has been called on a server, it may not be reused;
+// future calls to methods such as Serve will return ErrServerClosed.
+func (wnx *Wenex) Shutdown(timeout time.Duration) error {
+	var (
+		err [2]error
+		wg  sync.WaitGroup
+	)
+
+	for i := 0; i <= 1; i++ {
+		if wnx.servers[i] != nil {
+			wg.Add(1)
+
+			go func(i int) {
+				defer wg.Done()
+
+				ctx, cancel := context.WithTimeout(context.Background(), timeout)
+				defer cancel()
+
+				err[i] = wnx.servers[i].Shutdown(ctx)
+			}(i)
+		}
+	}
+
+	wg.Wait()
+
+	for _, err := range err {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (wnx *Wenex) fixEmptyChain() {
